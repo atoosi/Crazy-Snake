427b74d4a7557d9c3b735b7c3a6c2521283e0dd5
alireza <ali_ghanadan@hotmail.com>
2017-10-19 23:39:28 -0700

    first commit

---
+++ Snake/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Mono.Samples.Snake
+{
+    [Activity(Label = "مار دیوانه", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.main);
+            var btnEnter = FindViewById<Button>(Resource.Id.btnLogin);
+            btnEnter.Click += (sender, e) =>
+            {
+               
+                Intent intent = new Intent(this, typeof(SnakeActivity));
+                StartActivity(intent);
+            };
+
+        }
+    }
+}
